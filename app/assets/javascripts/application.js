f1f505802fd8c5a91ff4270a7f5693fe13fbb5ae
hscott <hilliards@gmail.com>
2014-01-31 12:35:35 -0700

    Add Users and login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
-//= require turbolinks
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
+//= require turbolinks
 //= require_tree .
 
 
