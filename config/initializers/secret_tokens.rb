bb61e1bff0a3faeb00136fc6ef50614995744529
hscott <hilliards@gmail.com>
2014-02-02 09:54:39 -0700

    Add secret key base

--- config/initializers/secret_tokens.rb
+++ config/initializers/secret_tokens.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Tea2::Application.config.secret_token = '37feafdae05df1a6fb65abab43a51d567f4eefe880d29143cd6e790d6b3a314b24d158088f6e2d9c8b07df4c59cee27b43fe4a1e463c99f3072a68a3bcf90ab2'
+Tea2::Application.config.secret_key_base = '37feafdae05df1a6fb65abab43a51d567f4eefe880d29143cd6e790d6b3a314b24d158088f6e2d9c8b07df4c59cee27b43fe4a1e463c99f30
